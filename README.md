1054406d1a091c31e325e1464f01e61de70e599d
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2023-05-04 13:44:12 +0200

    Scanning chapter

---
+++ README.md
@@ -0,0 +1,3 @@
+# Crafting interpreters jlox repo
+
+This is learning repo made following the code from http://craftinginterpreters.com/ and the challenges that were given at the end of the chapters.
---
+++ src/main/java/com/kebab/Scanner.java
@@ -0,0 +1,192 @@
+package com.kebab;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>(); 
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("func", TokenType.FUNC);
+        keywords.put("if", TokenType.IF);
+        keywords.put("nil", TokenType.NIL);
+        keywords.put("or", TokenType.OR);
+        keywords.put("print", TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super", TokenType.SUPER);
+        keywords.put("this", TokenType.THIS);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("while", TokenType.WHILE);
+    }
+
+	public Scanner(String source) {
+        this.source = source;
+	}
+
+	public List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+		return tokens;
+	}
+
+	private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(TokenType.LEFT_PARENTHESIS); break;
+            case ')': addToken(TokenType.RIGHT_PARANTHESIS); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '*': addToken(TokenType.STAR); break;
+            case '!': addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+            case '=': addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+            case '>': addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+            case '<': addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+            case '/':
+                if (match('/')) {
+                    // This is a comment
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else if (match('*')) {
+                    // This is a multi-line comment
+                    while (peek() != '*' && peekNext() != '/' && !isAtEnd()) {
+                        if (peek() == '\n') line++;
+                        advance();
+                    }
+                    // Skip */
+                    advance(); advance();
+                }
+                else {
+                    addToken(TokenType.SLASH);
+                }
+            // Ignore whitespaces
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"': string(); break;
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    App.error(line, "Unexpected character.");
+                }
+                break;
+        }
+	}
+
+    private boolean isDigit(char c) {
+        return '0' <= c && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+    }
+    
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+	private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            App.error(line, "Unterminated string.");
+            return;
+        }
+
+        // Closing "
+        advance();
+
+        // Don't get the quotes as part of the value
+        String value = source.substring(start + 1, current - 1);
+        addToken(TokenType.STRING, value);
+	}
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume .
+            advance();
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = TokenType.IDENTIFIER;
+        addToken(type);
+    }
+
+	private boolean isAtEnd() {
+		return current >= source.length();
+	}
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
