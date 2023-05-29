261469df8a6f88d5610315c670e4c35217f133bf
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2023-05-29 13:20:00 +0200

    Optimize visitCalLExpr

--- src/main/java/com/kebab/Interpreter.java
+++ src/main/java/com/kebab/Interpreter.java
@@ -130,14 +130,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	public Object visitCallExpr(Call expr) {
         Object callee = evaluate(expr.callee);
 
-        List<Object> arguments = new ArrayList<>();
-        for (Expr argument : expr.arguments) {
-            arguments.add(evaluate(argument));
-        }
-
         if (!(callee instanceof LoxCallable)) {
             System.out.println(callee);
             throw new RuntimeError(expr.paren, "Can only call functions and classes");
+        }
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
         }
         
         LoxCallable function = (LoxCallable) callee;
